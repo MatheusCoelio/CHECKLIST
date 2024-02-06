25b16e6084d63cfd631b0e8cf95bf93d9b6c9bfb
MatheusCoelio <matheusc2015@outlook.com>
2024-02-06 18:08:13 -0300

    alteração

--- checklistJS.js
+++ checklistJS.js
@@ -1,5 +1,7 @@
-var body = document.body;
+/// FUNÇÃO MUDAR COR
+var body = document.body; 
 var isBlue = false;
+
  
   function changeColor() {
     if (isBlue){
@@ -17,3 +19,22 @@ var isBlue = false;
     }
 
  }
+ 
+// FUNÇÃO TASKLIST
+
+var tasklist = document.getElementsById('tasklist'); //faz a variavel receber o campo onde ficarão as tarefas
+var taskinput = document.getElementsByClassName('dgt-tarefa'); //faz a variavel receber o campo de "digite sua tarefa..."
+
+function addtarefa () {
+  var tasktext = taskinput.Value.trim ();
+  if (tasktext !== ""){
+    var li = document.createElement("li");
+    li.innerHTML = `
+    <span>${tasktext}</span>
+    <button class ="btnEdit" onclick="edittask(this)> Editar</button>
+    <button class ="btnDelete" onclick="deletetask(this)> Remover</button>
+    `;
+    tasklist.appendChild(li);
+    taskinput.value = "";
+  }
+}
